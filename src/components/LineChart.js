44c0d09dd218b6675aa062e5f7c33fd70d168166
vicctor <artur.keska@gmail.com>
2024-06-10 23:12:25 +0200

    basic version of visualisation

---
+++ src/components/LineChart.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+
+function LineChart({ diagramLabel, chartData }) {
+  return (
+    <div className="chart-container">
+      <h3 style={{ textAlign: "center" }}>{diagramLabel}</h3>
+      <Line
+        data={chartData}
+        options={{
+          plugins: {
+            title: {
+              display: false
+            },
+            legend: {
+              display: false
+            },
+            pointBorderWidth: 0
+          }
+        }}
+      />
+    </div>
+  );
+}
+export default LineChart;
